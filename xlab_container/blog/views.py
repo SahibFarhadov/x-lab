37bd65cf73ba2f553e46ab83eefe7acf1d85cbee
Sahib <sahibcebrayilli@gmail.com>
2024-01-22 14:46:32 +0400

    Index sehifesinde cox oxunulan meqale bolumunde kateqoriyalar tenizmlendi

--- xlab_container/blog/views.py
+++ xlab_container/blog/views.py
@@ -4,10 +4,19 @@ from .models import Kateqoriya,Blog
 # Create your views here.
 
 def index(request):
+    # cox oxunulan blogu sehifeye list olaraq gondereceyik
+    # birincisi elementin ozu ikincisi novbeti elementin olub-olmadigini gosterecek
+    cox_oxunulan_kateqoriya = Blog.objects.get(id=2).blog_kateqoriya.all()
+    cox_oxunulan_kateqoriya_list = list()
+    for element in cox_oxunulan_kateqoriya:
+        cox_oxunulan_kateqoriya_list.append(list((element,True)))
+    cox_oxunulan_kateqoriya_list[len(cox_oxunulan_kateqoriya_list)-1][1] = False
+
     context = {
         'kateqoriyalar': Kateqoriya.objects.all(),
         'yazilar' : Blog.objects.all(),
-        'cox_oxunulan': Blog.objects.get(id=1),
+        'cox_oxunulan': Blog.objects.get(id=2),
+        'cox_oxunulan_kateqoriya_list':cox_oxunulan_kateqoriya_list,
         'yeni_yazilar' : Blog.objects.filter(yeni_mi=True),
     }
     return render(request,'blog/index.html',context)
