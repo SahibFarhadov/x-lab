b61da3528002336c3142f8245be1a23549921bb7
Sahib <sahibcebrayilli@gmail.com>
2024-01-24 02:25:32 +0400

    Qeydiyyat sehifesinde parolu goster funksiyasi yazildi. javascript elave edildi

---
+++ xlab_container/account/static/account/js/show_password.js
@@ -0,0 +1,16 @@
+// sifreni goster elementin alinmasi
+let sifreni_goster_element = document.getElementById("id_sifreni_goster");
+let id_password1 = document.getElementById("id_password1");
+let id_password2 = document.getElementById("id_password2");
+
+sifreni_goster_element.onchange = function(){
+    if(sifreni_goster_element.checked){
+        id_password1.setAttribute("type","text");
+        id_password2.setAttribute("type","text");
+    }
+    else{
+        id_password1.setAttribute("type","password");
+        id_password2.setAttribute("type","password");
+    }
+}
+
