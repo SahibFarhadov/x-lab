c60ffc0ee73f55ca6c79ae506b2d75adfd45bdb0
Sahib <sahibcebrayilli@gmail.com>
2024-01-26 17:26:35 +0400

    Author modelinə author_uuid sahəsi əlavə edildi.

--- xlab_container/account/models.py
+++ xlab_container/account/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .managers import AuthorManager
 from django.utils.translation import gettext_lazy as _
+import uuid
 
 # Create your models here.
 
@@ -11,8 +12,14 @@ class Author(AbstractUser):
         verbose_name_plural = 'Müəlliflər'
     username = None
     email = models.EmailField(_("Email"),unique=True)
+    author_uuid = models.UUIDField(editable=False,null=True,default=uuid.uuid4)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     objects = AuthorManager()
+    
     def __str__(self):
         return self.email
+
+    def save(self,*args,**kwargs):
+        self.author_uuid = uuid.uuid4()
+        super().save(*args,**kwargs)
